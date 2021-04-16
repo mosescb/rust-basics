a32e69140816cc8f9a9c5e96a3af290ec3082302
Moses Christopher Bollavarapu <moseschristopherb@gmail.com>
2021-04-16 13:40:12 +0200

    Add structs and file operartions
    Signed-off-by: Moses Christopher Bollavarapu <moseschristopherb@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@
  *
  */
 
+use std::io::prelude::*;
+use std::env;
+use std::fs;
 
 fn main() {
     // Mutable vs Immutable
@@ -43,6 +46,28 @@ fn main() {
 
     // Calculate minimum, maximum, average from an array of integers
     calc_min_max_avg();
+
+    // Strings and slices
+    let mut str_message :String = String::from("    Hello this is a new string");
+    str_message.push_str(" which got appended by another string     ");
+    println!("{}\n", str_message);
+
+    // Structs by-default stored on stack
+    struct_basics();
+
+    // Tuple struct
+    tuple_struct_basics();
+
+    // Rectangle struct
+    rectangle_struct();
+
+    // Printing arguments passed
+    for (index, arg) in env::args().enumerate() {
+        println!("arg {} is {}", index, arg);
+    }
+
+    // File operations
+    file_ops();
 }
 
  /*
@@ -314,4 +339,198 @@ fn calc_min_max_avg() {
 
     f32_avg = f32_avg / i32_arr_integers.len() as f32;
     println!("MIN= {}\t MAX= {}\t AVG= {}\n", i32_min, i32_max, f32_avg);
+}
+
+/*
+* Struct
+*
+*/
+#[derive(Debug)] // Needed to print data using {:?}
+#[derive(Clone)] // Needed to clone strings
+struct Person {
+    name: String,
+    gender: String,
+    age: u8,
+    height: f32,
+    weight: f32
+}
+
+// Struct methods & associated functions
+impl Person {
+    // methods contain &self
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+    fn get_age(&self) -> u8 {
+        self.age
+    }
+    fn set_age(&mut self, new_age :u8) {
+        self.age = new_age;
+    }
+
+    // associated functions do not have &self argument
+    fn new(entered_name: &str) -> Person {
+        Person {
+            name: String::from(entered_name),
+            gender: String::from("Undefined"),
+            age: 0,
+            height: 0.0,
+            weight: 0.0
+        }
+    }
+}
+
+/*
+ * Struct basics
+ *
+ */
+fn struct_basics() {
+    let Person1 = Person{
+        name: String::from("Chris"),
+        gender: String::from("Male"),
+        age: 95,
+        height: 180.01,
+        weight: 78.65
+    };
+
+    let Person2 = Person{
+        name: String::from("Moses"),
+        gender: String::from("Male"),
+        ..Person1  // Copy rest of the values from Person1 to Person2
+    };
+
+    let mut Person3 = Person{
+        name: String::from("Steve"),
+        ..Person1.clone()  // Copy rest of the values from Person1 to Person3 (even string as clone)
+    };
+
+    let mut Person4 = Person::new("Lisa");
+
+    Person3.age = 36;
+    Person4.gender = String::from("Female");
+
+    println!("Data from struct: \nname: {}\tage: {}\tgender: {}\n", Person1.name, Person1.age, Person1.gender);
+
+    println!("Print struct data using Debug print\n");
+    println!("{:?}", Person1);
+    println!("{:?}", Person2);
+    println!("{:?}", Person3);
+    println!("{:?}\n", Person4);
+
+    // let person3_name = Person3.get_name(); // --> Produces error as it is called as immutable first
+    // Update person3 age to 45
+    Person3.set_age(45); // Mutable call
+    let person3_name = Person3.get_name(); // --> No error here because first call is mutable
+    println!("Person3 details from methods: name = {}\t age = {}\n", person3_name, Person3.get_age());
+}
+
+// Tuple structs
+struct Color(u8, u8, u8);   // Red Green Blue
+struct Point3D(u8, u8, u8); // X,Y,Z Coordinates
+struct Point2D(u8, u8);     // X,Y Coordinates
+
+/*
+ * Sample function with tuple struct as an argument
+ *
+ */
+fn get_point3d_x(px :&Point3D) -> u8 {
+    px.0
+}
+
+/*
+ * Tuple struct basics
+ *
+ */
+fn tuple_struct_basics() {
+    let col_red = Color(255,0,0);
+    let point_3d = Point3D(3,6,1);
+    let point_2d = Point2D(12, 5);
+
+    println!("Red : {}", col_red.0);
+    println!("X in 3d point : {}", get_point3d_x(&point_3d));
+    println!("Z in 3d point : {}", point_3d.2);
+    println!("Y in 2d point : {}\n", point_2d.1);
+}
+
+/*
+ * Rectangle struct
+ *
+ */
+fn rectangle_struct() {
+    struct Rectangle {
+        width :f64,
+        height :f64
+    }
+
+    impl Rectangle {
+        fn get_area(&self) -> f64 {
+            self.width * self.height
+        }
+        fn scale(&mut self, mult :f64) {
+            self.width *= mult;
+            self.height *= mult;
+        }
+
+        fn new(input_width :f64, input_height :f64) -> Rectangle {
+            Rectangle {
+                width: input_width,
+                height: input_height
+            }
+        }
+    }
+
+    let mut rect1 = Rectangle::new(10.0, 2.5);
+    println!("Width: {}\t Height: {}", rect1.width, rect1.height);
+    assert_eq!(rect1.get_area(), 25.0);
+    rect1.scale(2.0);
+    println!("Width: {}\t Height: {}\n", rect1.width, rect1.height);
+    assert_eq!(rect1.get_area(), 100.0);
+    println!("Rectangle struct works as expected");
+}
+
+/*
+ * File operations
+ *
+ */
+fn file_ops() {
+    // Write data to file
+    let name_of_text_file = String::from("text-data/names.txt");
+    let mut names_data = String::new();
+    names_data.push_str("Moses\n");
+    names_data.push_str("Christopher\n");
+    names_data.push_str("CarrotPi");
+    fs::write(&name_of_text_file, names_data).unwrap();  // needs std::fs
+
+    // Append data to file
+    let mut file_with_append = fs::OpenOptions::new().append(true).open(&name_of_text_file).unwrap();
+    file_with_append.write(b"\nSteve").unwrap(); // needs std::io::prelude
+
+    // Read complete file, line by line
+    let file_content = fs::read_to_string(&name_of_text_file).unwrap();
+    for line in file_content.lines()
+    {
+        println!("line-by-line: {}", line);
+    }
+
+    // Commandline check with files  --> needs std::env
+    if env::args().enumerate().len() <= 2 {
+        println!("Need two arguments to process");
+        println!("\nUsage: cargo run path-to-file search-text");
+        println!("Example: cargo run {} Moses\n",name_of_text_file);
+        return;
+    }
+    // Usage: cargo run text-data/names.txt Moses
+    let read_path = env::args().nth(1).unwrap();
+    let match_string = env::args().nth(2).unwrap();
+    // println!("Read path is {}", read_path);
+
+    let file_content = fs::read_to_string(&read_path).unwrap();
+    for line in file_content.lines()
+    {
+        if line.contains(&match_string) {
+            println!("Found a match");
+            break;
+        }
+        // println!("No match found");
+    }
 }
